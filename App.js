4bccb022595b4c11dea848c1cf0c82e2dd15cf56
alestor123 <alestoraldous@gmail.com>
2020-12-26 21:40:29 +0530

    added main code

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
 var option = require('minimist')(process.argv.slice(2)),
 message = option.m || option.message || 'Take A Break',
+time = option.t || option.time || 1,
 notify = require('node-notifier');
+setInterval(() => {  
+notify.notify({
+    title: option.h || option.head || 'Take A Break',
+    message: message
+  });
+}
+, time*60*1000);
